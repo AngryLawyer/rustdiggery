571e4a0c743365b2ad0d816a103053e2001f5bd6
Tony Aldridge <tony@dabapps.com>
2017-05-27 11:04:34 +0100

    Started plugging in Scene stack

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ extern crate sdl2_engine_helpers;
 
 //pub mod scene;
 //pub mod scene_manager;
-//pub mod title_scene;
+pub mod title_scene;
 //pub mod game_scene;
 //pub mod entity;
 //pub mod keyhandler;
 use sdl2_engine_helpers::game_loop::GameLoop;
 use sdl2_engine_helpers::scene::SceneStack;
 use sdl2::pixels::Color;
+
+use title_scene::TitleScene;
 
 fn main() {
     let sdl_context = sdl2::init().expect("Could not initialize SDL context");
@@ -26,13 +28,18 @@ fn main() {
         .expect("Could not get Canvas");
 
     let mut scene_stack = SceneStack::new();
+    scene_stack.push(TitleScene::new());
 
     let mut game_loop = GameLoop::new(60);
     game_loop.run(|frame_number| {
-        let color = (frame_number % 256) as u8;
-        canvas.set_draw_color(Color::RGB(color, color, color));
-        canvas.clear();
-        canvas.present();
-        false
+        if scene_stack.is_empty() {
+            true
+        } else {
+            let color = (frame_number % 256) as u8;
+            canvas.set_draw_color(Color::RGB(color, color, color));
+            canvas.clear();
+            canvas.present();
+            false
+        }
     });
 }
--- src/title_scene.rs
+++ src/title_scene.rs
@@ -1,20 +1,19 @@
-use scene::{Scene, BoxedScene, SceneCommand};
-use piston_window::*;
+use sdl2_engine_helpers::scene::{BoxedScene, Scene, SceneChangeEvent};
 
 pub struct TitleScene;
 
 impl TitleScene {
-    pub fn new() -> BoxedScene {
+    pub fn new() -> BoxedScene<(), (), ()> {
         Box::new(TitleScene)
     }
 }
 
-impl Scene for TitleScene {
+impl Scene<(), (), ()> for TitleScene {
 
-    fn handle_event(&mut self, e: &PistonWindow) -> Option<SceneCommand> {
-        e.draw_2d(|_c, g| {
-            clear([0.0, 0.0, 0.0, 1.0], g);
-        });
+    fn render(&self, renderer: &mut (), engine_data: &mut (), tick: u64) {
+    }
+
+    fn handle_event(&mut self, event: &(), renderer: &mut (), engine_data: &mut (), tick: u64) -> Option<SceneChangeEvent<(), (), ()>> {
         None
     }
 }
