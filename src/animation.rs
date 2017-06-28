b8f8df7aef15920fd4b890810dbe64fd8e7d9502
Tony Aldridge <tony@dabapps.com>
2017-06-28 18:03:43 +0100

    Basic animation frames structure

--- src/animation.rs
+++ src/animation.rs
@@ -1,3 +1,4 @@
+use sdl2::rect::Rect;
 use std::collections::HashMap;
 
 pub struct AnimationSet {
@@ -17,7 +18,29 @@ impl AnimationSet {
 }
 
 pub struct Animation {
+    frames: Vec<AnimationFrame>
 }
 
-struct AnimationFrame {
+impl Animation {
+    pub fn new() -> Animation {
+        Animation {
+            frames: vec![]
+        }
+    }
+
+    pub fn add(&mut self, frame: AnimationFrame) {
+        self.frames.push(frame);
+    }
+}
+
+pub struct AnimationFrame {
+    source_bounds: Rect
+}
+
+impl AnimationFrame {
+    pub fn new(source: &Rect) -> AnimationFrame {
+        AnimationFrame {
+            source_bounds: source.clone()
+        }
+    }
 }
