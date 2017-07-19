5769a5ee91312b018662f360b01d2c2f6886f421
Tony Aldridge <tony@dabapps.com>
2017-07-19 06:37:15 +0100

    Standard cell states

--- src/cell_state.rs
+++ src/cell_state.rs
@@ -22,6 +22,260 @@ pub fn get_tileset_sprite(adjacents: &[bool; 8]) -> (u32, u32, FlipContext) {
         adjacents[7],
     );
     match adjacent_tiles {
+        // 0, 0 - Unused ramp
+        // 0, 1 - Unused ramp
+        (
+            true,  false,  false,
+            false,         false,
+            true,  false,  true
+        ) => {
+            (0, 2, FlipContext::FlipNone)  // 3 corners
+        },
+        (
+            false, false, true,
+            false,        false,
+            true, false, true,
+        ) => {
+            (0, 2, FlipContext::FlipHorizontal)  // 3 corners
+        },
+        (
+            true,  false,  true,
+            false,         false,
+            true,  false,  false
+        ) => {
+            (0, 3, FlipContext::FlipNone)  // 3 corners
+        },
+        (
+            true,  false, true,
+            false,       false,
+            false, false, true,
+        ) => {
+            (0, 3, FlipContext::FlipHorizontal)  // 3 corners
+        },
+        (
+            _,     true,  _,
+            true,         false,
+            _,     false, false
+        ) => {
+            (1, 0, FlipContext::FlipNone)  // Top-left
+        },
+        (
+            _,     true,  _,
+            false,        true,
+            false, false, _
+        ) => {
+            (1, 0, FlipContext::FlipHorizontal)  // Top-right
+        },
+        (
+            _,     false,  false,
+            true,         false,
+            _,     false, false
+        ) => {
+            (1, 1, FlipContext::FlipNone)  // Left
+        },
+        (
+            false,  false, _,
+            false,         true,
+            false,  false, _
+        ) => {
+            (1, 1, FlipContext::FlipHorizontal)  // Right
+        },
+        (
+            _,     false, false,
+            true,         false,
+            _,     true , _
+        ) => {
+            (1, 2, FlipContext::FlipNone)  // Bottom-left
+        },
+        (
+            false, false, _,
+            false,        true,
+            _,     true,  _
+        ) => {
+            (1, 2, FlipContext::FlipHorizontal)  // Bottom-right
+        },
+        (
+            _,     true,  _,
+            true,         false,
+            _,     true,  _
+        ) => {
+            (1, 3, FlipContext::FlipNone)  // Left nub
+        },
+        (
+            _,     true,  _,
+            false,        true,
+            _,     true,  _
+        ) => {
+            (1, 3, FlipContext::FlipHorizontal)  // Right nub
+        },
+        (
+            true,  false, false,
+            false,        false,
+            false, false, false
+        ) => {
+            (2, 0, FlipContext::FlipNone)  // Inside elbow top left
+        },
+        (
+            false, false, true,
+            false,        false,
+            false, false, false
+        ) => {
+            (2, 0, FlipContext::FlipHorizontal)  // Inside elbow top right
+        },
+        (
+            true,  false, false,
+            false,        false,
+            true, false, false
+        ) => {
+            (2, 1, FlipContext::FlipNone)  // Left corners
+        },
+        (
+            false, false, true,
+            false,        false,
+            false, false, true
+        ) => {
+            (2, 1, FlipContext::FlipHorizontal)  // Right corners
+        },
+        (
+            false, false, false,
+            false,        false,
+            true, false, false
+        ) => {
+            (2, 2, FlipContext::FlipNone)  // Inside elbow bottom left
+        },
+        (
+            false, false, false,
+            false,        false,
+            false, false, true
+        ) => {
+            (2, 2, FlipContext::FlipHorizontal)  // Inside elbow bottom right
+        },
+        (
+            _,     true,  _,
+            true,         false,
+            _,     false, true
+        ) => {
+            (2, 3, FlipContext::FlipNone)  // Top-left bend
+        },
+        (
+            _,      true,  _,
+            false,         true,
+            true,    false, _
+        ) => {
+            (2, 3, FlipContext::FlipHorizontal)  // Top-right bend
+        },
+        (
+            _,    true,  _,
+            false,       false,
+            true, false, false
+        ) => {
+            (3, 0, FlipContext::FlipNone)  // T
+        },
+        (
+            _,    true,   _,
+            false,        false,
+            false, false, true
+        ) => {
+            (3, 0, FlipContext::FlipHorizontal)  // T
+        },
+        (
+            false, false, true,
+            false,        false,
+            true, false, false
+        ) => {
+            (3, 1, FlipContext::FlipNone)  // Diagonal
+        },
+        (
+            true,  false, false,
+            false,        false,
+            false, false, true,
+        ) => {
+            (3, 1, FlipContext::FlipHorizontal)  // Diagonal
+        },
+        (
+            true, false, false,
+            false,       false,
+            _,    true,   _
+        ) => {
+            (3, 2, FlipContext::FlipNone)  // T
+        },
+        (
+            false, false, true,
+            false,        false,
+            _,     true,  _
+        ) => {
+            (3, 2, FlipContext::FlipHorizontal)  // T
+        },
+        (
+            _,     false, true,
+            true,         false,
+            _,     true , _
+        ) => {
+            (3, 3, FlipContext::FlipNone)  // Bottom-left
+        },
+        (
+            true,  false, _,
+            false,        true,
+            _,      true , _
+        ) => {
+            (3, 3, FlipContext::FlipHorizontal)  // Bottom-left
+        },
+        (
+            true, false, _,
+            false,        true,
+            false, false, _
+        ) => {
+            (4, 0, FlipContext::FlipNone)  // Side missing top corner
+        },
+        (
+            _,    false,  true,
+            true,        false,
+            _,    false, false,
+        ) => {
+            (4, 0, FlipContext::FlipHorizontal)  // Side missing top corner
+        },
+        (
+            true, false, _,
+            false,        true,
+            true, false, _
+        ) => {
+            (4, 1, FlipContext::FlipNone)  // Right T
+        },
+        (
+            _,     false, true,
+            true,         false,
+            _,     false, true,
+        ) => {
+            (4, 1, FlipContext::FlipHorizontal)  // Left T
+        },
+        (
+            false, false, _,
+            false,        true,
+            true, false, _
+        ) => {
+            (4, 2, FlipContext::FlipNone)  // Side missing bottom corner
+        },
+        (
+            _,    false, false,
+            true,        false,
+            _,    false, true
+        ) => {
+            (4, 2, FlipContext::FlipHorizontal)  // Side missing bottom corner
+        },
+        (
+            true,  false, true,
+            false,        false,
+            false,     false, false
+        ) => {
+            (4, 3, FlipContext::FlipNone)  // T
+        },
+        (
+            false, false, false,
+            false,        false,
+            true,  false, true
+        ) => {
+            (4, 3, FlipContext::FlipVertical)  // T
+        },
         (
             _,     true,  _,
             true,         true,
@@ -52,66 +306,10 @@ pub fn get_tileset_sprite(adjacents: &[bool; 8]) -> (u32, u32, FlipContext) {
         },
         (
             _,     true,  _,
-            true,         false,
-            _,     true,  _
-        ) => {
-            (1, 3, FlipContext::FlipNone)  // Left nub
-        },
-        (
-            _,     true,  _,
             false,        false,
-            _,     true,  _
+            true,  false, true,
         ) => {
-            (7, 1, FlipContext::FlipNone)  // Horizontal bar
-        },
-        (
-            _,     true,  _,
-            false,        true,
-            _,     true,  _
-        ) => {
-            (1, 3, FlipContext::FlipHorizontal)  // Right nub
-        },
-        (
-            _,     true,  _,
-            true,         false,
-            _,     false, false
-        ) => {
-            (1, 0, FlipContext::FlipNone)  // Top-left
-        },
-        (
-            _,     false,  false,
-            true,         false,
-            _,     false, false
-        ) => {
-            (1, 1, FlipContext::FlipNone)  // Left
-        },
-        (
-            _,     false, false,
-            true,         false,
-            _,     true , _
-        ) => {
-            (1, 2, FlipContext::FlipNone)  // Bottom-left
-        },
-        (
-            _,     true,  _,
-            false,        true,
-            false, false, _
-        ) => {
-            (1, 0, FlipContext::FlipHorizontal)  // Top-right
-        },
-        (
-            false,  false, _,
-            false,         true,
-            false,  false, _
-        ) => {
-            (1, 1, FlipContext::FlipHorizontal)  // Right
-        },
-        (
-            false, false, _,
-            false,        true,
-            _,     true,  _
-        ) => {
-            (1, 2, FlipContext::FlipHorizontal)  // Bottom-right
+            (6, 0, FlipContext::FlipNone)  // T
         },
         (
             _, true,      _,
@@ -120,117 +318,13 @@ pub fn get_tileset_sprite(adjacents: &[bool; 8]) -> (u32, u32, FlipContext) {
         ) => {
             (6, 1, FlipContext::FlipNone)  // Top
         },
+        // Skip 6, 2 - centre block
         (
             false, false, false,
             _,            _,
             _, true,      _
         ) => {
             (6, 3, FlipContext::FlipNone)  // Bottom
-        },
-        (
-            _,    true,   _,
-            false,        false,
-            false, false, true
-        ) => {
-            (3, 0, FlipContext::FlipHorizontal)  // T
-        },
-        (
-            _,    true,  _,
-            false,       false,
-            true, false, false
-        ) => {
-            (3, 0, FlipContext::FlipNone)  // T
-        },
-        (
-            true, false, false,
-            false,       false,
-            _, true,  false
-        ) => {
-            (3, 2, FlipContext::FlipNone)  // T
-        },
-        (
-            false, false, true,
-            false,        false,
-            false, true,  _
-        ) => {
-            (3, 2, FlipContext::FlipHorizontal)  // T
-        },
-        (
-            true,  false, false,
-            false,        false,
-            false, false, false
-        ) => {
-            (2, 0, FlipContext::FlipNone)  // Inside elbow top left
-        },
-        (
-            false, false, true,
-            false,        false,
-            false, false, false
-        ) => {
-            (2, 0, FlipContext::FlipHorizontal)  // Inside elbow top right
-        },
-        (
-            false, false, false,
-            false,        false,
-            true, false, false
-        ) => {
-            (2, 2, FlipContext::FlipNone)  // Inside elbow bottom left
-        },
-        (
-            false, false, false,
-            false,        false,
-            false, false, true
-        ) => {
-            (2, 2, FlipContext::FlipHorizontal)  // Inside elbow bottom right
-        },
-        (
-            false, false, true,
-            false,        false,
-            true, false, false
-        ) => {
-            (3, 1, FlipContext::FlipNone)  // Diagonal
-        },
-        (
-            true,  false, false,
-            false,        false,
-            false, false, true,
-        ) => {
-            (3, 1, FlipContext::FlipHorizontal)  // Diagonal
-        },
-        (
-            false, false, _,
-            false,        true,
-            true, false, _
-        ) => {
-            (4, 2, FlipContext::FlipNone)  // Side missing bottom corner
-        },
-        (
-            _,    false, false,
-            true,        false,
-            _,    false, true
-        ) => {
-            (4, 2, FlipContext::FlipHorizontal)  // Side missing bottom corner
-        },
-        (
-            true, false, _,
-            false,        true,
-            false, false, _
-        ) => {
-            (4, 0, FlipContext::FlipNone)  // Side missing top corner
-        },
-        (
-            _,    false,  true,
-            true,        false,
-            _,    false, false,
-        ) => {
-            (4, 0, FlipContext::FlipHorizontal)  // Side missing top corner
-        },
-        (
-            _,     true,  _,
-            false,        false,
-            true,  false, true,
-        ) => {
-            (6, 0, FlipContext::FlipNone)  // T
         },
         (
             true,  false, true,
@@ -240,46 +334,18 @@ pub fn get_tileset_sprite(adjacents: &[bool; 8]) -> (u32, u32, FlipContext) {
             (7, 0, FlipContext::FlipNone)  // Inverse T
         },
         (
-            true,  false, true,
+            _,     true,  _,
             false,        false,
-            _,     false, _
+            _,     true,  _
         ) => {
-            (4, 3, FlipContext::FlipNone)  // T
+            (7, 1, FlipContext::FlipNone)  // Horizontal bar
         },
         (
-            _,     false, _,
+            true,  false, true,
             false,        false,
             true,  false, true
         ) => {
-            (4, 3, FlipContext::FlipVertical)  // T
-        },
-        (
-            _,     true,  _,
-            true,         false,
-            _,     false, true
-        ) => {
-            (2, 3, FlipContext::FlipNone)  // Top-left bend
-        },
-        (
-            _,      true,  _,
-            false,         true,
-            true,    false, _
-        ) => {
-            (2, 3, FlipContext::FlipHorizontal)  // Top-right bend
-        },
-        (
-            _,     false, true,
-            true,         false,
-            _,     true , _
-        ) => {
-            (3, 3, FlipContext::FlipNone)  // Bottom-left
-        },
-        (
-            true,  false, _,
-            false,        true,
-            _,      true , _
-        ) => {
-            (3, 3, FlipContext::FlipHorizontal)  // Bottom-left
+            (7, 2, FlipContext::FlipNone)  // +
         },
         (
             _, _, _,
